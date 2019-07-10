38441f4db7b380852365fc646c6c68f649a73a46
zhuangrong2025 <42589963@qq.com>
2019-07-10 16:38:32 +0800

    .

--- eframe-component/src/main/resources/static/AutoflowEditor/iGet.js
+++ eframe-component/src/main/resources/static/AutoflowEditor/iGet.js
@@ -45,19 +45,87 @@ var env = {};
  (function(){
    var MenuToolBar = mse.BaseModule.extend({
      constructor: function(){
-       this.init()
+        this.init()
      },
      init: function(){
-       this.bindEvent()
+        this.bindEvent()
      },
      bindEvent: function(){
        // 绑定菜单事件
        $('.menu a').click(function(){
-         var action = $(this).attr('data-action')
-         var fn = env.actions.get(action)
-         fn && fn()
+           var action = $(this).attr('data-action')
+           var fn = env.actions.get(action)
+           fn && fn()
        })
      }
    })
    env.MenuToolbar = new MenuToolBar();
+ })();
+
+ /**
+  * mse.apply对象,赋值数据
+  */
+(function(){
+  var step = {id: "0", sec: "流程块"},
+      props = {id: "1", file: '123.ahk'}
+  var newObj = mse.apply(step, props);
+  console.log(newObj); // {id:1, sec: "流程块", file: '123.ahk'}
+})();
+
+/**
+ * 存储数据
+ * 1、普通的存取数据
+ * 2、window.sessionStorage的二次封装
+ */
+ (function(){
+   mse.datas = {}
+   var data = {}
+   function MemoryStorage(){}
+   MemoryStorage.prototype = {
+       getItem: function(key){
+           return data[key]
+       },
+       setItem: function(key, value){
+           if(typeof key !== 'string' || !key){
+               throw new Error("key is invalid")
+           }
+           data[key] = value
+           return this
+       }
+   }
+   mse.datas.MemoryStorage = new MemoryStorage()
+ })();
+
+ (function(){
+   mse.datas.Storage = function(){
+       var storage = window.sessionStorage;
+       if (!storage) {//不支持sessionStorage则使用内存缓存
+           storage = mse.data.MemoryStorage;
+       }
+       var methods = {
+           getItem: function (key) {
+               value = JSON.parse(storage.getItem(key));
+               return value;
+           },
+           setItem : function (key, value) {
+               if (typeof key !== 'string' || !key) {
+                   throw new Error('cannot call sui.data.Storage.set(), key is invalid');
+               }
+               storage.setItem(key, JSON.stringify(value));
+               return storage;
+           }
+       };
+       methods.get = methods.getItem;
+       methods.set = methods.setItem;
+       return methods;
+   }(); // 注意这里有(),是立即执行
+
+   mse.apply(mse, {
+       iStorage : function() {
+           return mse.datas.Storage
+       }
+   });
+   console.log(mse.iStorage().get('user2'));
+
+   // mse.iStorage().set('user2', 'zr2');
  })();
